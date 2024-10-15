5dd8db1a9b93993e76ff35c0f23cb2e4b1bf5a02
Victor Lawrence <viclawrence99@gmail.com>
2024-10-15 01:56:54 +0100

    this night

--- Screens/Protected/components/PopularComponent.js
+++ Screens/Protected/components/PopularComponent.js
@@ -1,28 +1,36 @@
-import {View, TouchableOpacity, Text} from "react-native";
+import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 const PopularComponent = ({ popular }) => {
-    return (
-      <View
-        style={{
-          padding: 5,
-          // flex: 1,
-          // backgroundColor:'red',
-          width: 180,
-        }}
-      >
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("Billboardclicked", { data: popular })
-          }
-        >
-          {/* <Image
-            resizeMode="cover"
-            source={{ uri: popular.image }}
-            style={styles.rectangleIcon3}
-          /> */}
-          <Text>{popular.location}</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
+  console.log(popular);
+  return (
+    <View
+      style={{
+        padding: 5,
+        flex: 1,
 
+        width: 180,
+      }}
+    >
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Billboardclicked", { data: popular })
+        }
+      >
+        <Image
+          resizeMode="cover"
+          // source={{ uri: popular.image }}
+          style={styles.rectangleIcon3}
+        />
+        <Text>{popular.location}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  rectangleIcon3: {
+    borderRadius: 10,
+    height: 150,
+    width: 159.5,
+  },
+});
 export default PopularComponent;
